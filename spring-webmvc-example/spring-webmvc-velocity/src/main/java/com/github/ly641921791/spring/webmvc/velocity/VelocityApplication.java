5f7b778f6ef6b9816073716cbed9b7c503bca8c2
ly570538 <wb-ly570538@alibaba-inc.com>
2019-07-01 19:06:39 +0800

    [Add] 提交Spring Boot 模本引擎Velocity示例

---
+++ spring-webmvc-example/spring-webmvc-velocity/src/main/java/com/github/ly641921791/spring/webmvc/velocity/VelocityApplication.java
@@ -0,0 +1,16 @@
+package com.github.ly641921791.spring.webmvc.velocity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wb-ly570538
+ */
+@SpringBootApplication
+public class VelocityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VelocityApplication.class, args);
+    }
+
+}
---
+++ spring-webmvc-example/spring-webmvc-velocity/src/main/java/com/github/ly641921791/spring/webmvc/velocity/controller/VelocityController.java
@@ -0,0 +1,24 @@
+package com.github.ly641921791.spring.webmvc.velocity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author wb-ly570538
+ */
+@Controller
+public class VelocityController {
+
+    @RequestMapping(path = "/hello")
+    public ModelAndView hello(Model model) {
+        ModelAndView modelAndView = new ModelAndView("index");
+
+        String hello = "Hello world";
+        model.addAttribute("hello", hello);
+
+        return modelAndView;
+    }
+
+}
